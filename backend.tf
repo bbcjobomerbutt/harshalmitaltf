dc224f5bfc093d15f878ae6627ea703e59a137db
Omer Butt <omer.butt@bbcjob.com>
2022-12-04 15:11:26 +0000

    remote state 2

--- backend.tf
+++ backend.tf
@@ -7,6 +7,15 @@ terraform {
   }
 }
 
+terraform {
+backend "azurerm" {
+    resource_group_name  = "bbcjob_tfstate_rg"
+    storage_account_name = "bbcjobremotestatetf"
+    container_name       = "bbcjobtfstate"
+    key                  = "terraform041222.tfstate"
+  }
+}
+
 provider "azurerm" {
   
   features {}
